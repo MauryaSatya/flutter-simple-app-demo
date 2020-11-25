0bc8483dcfb0edd40aaf124261d8fedbd123d592
UjjawalMaurya <chocolateboyz0011@gmail.com>
2020-11-25 19:34:06 +0530

    code cleaned

--- lib/main.dart
+++ lib/main.dart
@@ -27,55 +27,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-// class SplashScreen extends StatefulWidget {
-//   @override
-//   _SplashScreenState createState() => _SplashScreenState();
-// }
-
-// class _SplashScreenState extends State<SplashScreen> {
-//   @override
-//   void initState() {
-//     super.initState();
-//     new Future.delayed(
-//       Duration(seconds: 3),
-//       () {
-//         Navigator.push(
-//             context, MaterialPageRoute(builder: (context) => M1Screen()));
-//       },
-//     );
-//   }
-
-// @override
-// Widget build(BuildContext context) {
-//   return Container(
-//       decoration: BoxDecoration(
-//           gradient: LinearGradient(
-//               begin: Alignment.topLeft,
-//               end: Alignment.bottomRight,
-//               stops: [
-//             0.1,
-//             0.3,
-//             0.7,
-//             1
-//           ],
-//               colors: [
-//             Colors.green,
-//             Colors.blue,
-//             Colors.orange,
-//             Colors.pink
-//           ])),
-//       child: Center(
-//           child: Container(
-//               child: Card(
-//         color: Colors.orangeAccent,
-//         child: Image.asset(
-//           'images/clogo.png',
-//           fit: BoxFit.fill,
-//         ),
-//       ))));
-// }
-// }
-
 Color _color5 = Colors.blue[600];
 
 class M1Screen extends StatefulWidget {
@@ -169,124 +120,14 @@ class _M1ScreenState extends State<M1Screen> {
                             Navigator.of(context).pushNamed('/first');
                           },
                         ),
-
-// Column(
-                        // children: [
-                        // Container(
-                        //     height: 200,
-                        //     width: 200,
-                        //     child: InkWell(
-                        //         onTap: () {
-                        //           Navigator.pushNamed(context, '/first');
-                        //         },
-                        //         child: Material(
-                        //           color: _color5,
-                        //           elevation: 10.0,
-                        //           borderRadius: BorderRadius.circular(35.0),
-                        //           child: Container(
-//                                           child: Column(
-//                                         children: <Widget>[
-//                                           Padding(
-//                                             padding: EdgeInsets.all(8),
-//                                             child: Material(
-//                                               elevation: 5.0,
-//                                               borderRadius:
-//                                                   BorderRadius.circular(100.0),
-//                                               child: Container(
-//                                                   height: 150.0,
-//                                                   width: 150.0,
-//                                                   child: ClipOval(
-//                                                     child: Image.asset(
-//                                                         'images/basic.png'),
-//                                                   )),
-//                                             ),
-//                                           ),
-//                                           Center(
-//                                               child: Text(" Basic tutorial",
-//                                                   style: TextStyle(
-//                                                     fontSize: 20.0,
-//                                                     color: Colors.black,
-//                                                     fontFamily:
-//                                                         'LexendDeca-Regular',
-//                                                     fontWeight: FontWeight.w700,
-//                                                   )))
-//                                         ],
-//                                       )),
-//                                     )))
-//                           ],
-//                         ),
-
-//
-//
-                        ///
-//
+                        // Screen 2
                         NavCard(
                             assetName: 'images/qa.png',
                             onPressed: () {
                               Navigator.of(context).pushNamed('/second');
                             },
                             title: 'Exam Q/A'),
-
-                        // // Q/A SCREEN  2 SCREEN CARD AND ROUTE
-
-                        // Card(
-                        //   color: Colors.orange,
-                        //   child: Column(
-                        //     children: [
-                        //       Container(
-                        //         height: 200,
-                        //         width: 200,
-                        //         child: InkWell(
-                        //           onTap: () {
-                        //             Navigator.pushNamed(context, '/second');
-                        //           },
-                        //           child: Material(
-                        //             color: _color5,
-                        //             elevation: 10.0,
-                        //             borderRadius: BorderRadius.circular(35.0),
-                        //             child: Container(
-                        //               child: Column(
-                        //                 children: <Widget>[
-                        //                   Padding(
-                        //                       padding: EdgeInsets.all(8),
-                        //                       child: Material(
-                        //                         elevation: 5.0,
-                        //                         borderRadius:
-                        //                             BorderRadius.circular(100.0),
-                        //                         child: Container(
-                        //                             height: 150.0,
-                        //                             width: 150.0,
-                        //                             child: ClipOval(
-                        //                               child: Image.asset(
-                        //                                   'images/qa.png'),
-                        //                             )),
-                        //                       )),
-                        //                   Center(
-                        //                     child: Text(
-                        //                       " Exam Q/A",
-                        //                       style: TextStyle(
-                        //                         fontSize: 20.0,
-                        //                         color: Colors.black,
-                        //                         fontFamily: 'LexendDeca-Regular',
-                        //                         fontWeight: FontWeight.w700,
-                        //                       ),
-                        //                     ),
-                        //                   ),
-                        //                 ],
-                        //               ),
-                        //             ),
-                        //           ),
-                        //         ),
-                        //       )
-                        //     ],
-                        //   ),
-                        // ),
-//
-//
-//
-//
-//
-//
+                        // Screen 3
                         NavCard(
                             assetName: 'images/share.png',
                             onPressed: () {
@@ -294,129 +135,13 @@ class _M1ScreenState extends State<M1Screen> {
                                   'Checkout about cool and amazing app for learning C-language in very easy and understandable lanuguage https://play.google.com/pub:#############');
                             },
                             title: 'Share App'),
-
-                        // // SHERE SCREEN  3 SCREEN CARD AND ROUTE
-                        // Card(
-                        //   color: Colors.orange,
-                        //   child: Column(
-                        //     children: [
-                        //       Container(
-                        //         height: 200,
-                        //         width: 200,
-                        //         child: InkWell(
-                        //           onTap: () {
-                        //             Share.share(
-                        //                 'check out my app https://google.com/');
-                        //           },
-                        //           child: Material(
-                        //             color: _color5,
-                        //             elevation: 10.0,
-                        //             borderRadius: BorderRadius.circular(35.0),
-                        //             child: Container(
-                        //               child: Column(
-                        //                 children: <Widget>[
-                        //                   Padding(
-                        //                     padding: EdgeInsets.all(8),
-                        //                     child: Material(
-                        //                       elevation: 5.0,
-                        //                       borderRadius:
-                        //                           BorderRadius.circular(100.0),
-                        //                       child: Container(
-                        //                           height: 150.0,
-                        //                           width: 150.0,
-                        //                           child: ClipOval(
-                        //                             child: Image.asset(
-                        //                                 'images/share.png'),
-                        //                           )),
-                        //                     ),
-                        //                   ),
-                        //                   Center(
-                        //                     child: Text(
-                        //                       " Shere App",
-                        //                       style: TextStyle(
-                        //                         fontSize: 20.0,
-                        //                         color: Colors.black,
-                        //                         fontFamily: 'LexendDeca-Regular',
-                        //                         fontWeight: FontWeight.w700,
-                        //                       ),
-                        //                     ),
-                        //                   ),
-                        //                 ],
-                        //               ),
-                        //             ),
-                        //           ),
-                        //         ),
-                        //       )
-                        //     ],
-                        //   ),
-                        // ),
-
-// /
-// /
-// /
-// /
-// /
-// /
+                        // Screen 4
                         NavCard(
                             assetName: 'images/about.png',
                             onPressed: () {
                               Navigator.of(context).pushNamed('/third');
                             },
                             title: 'About Dev')
-
-                        // // ABOUT SCREEN  4 SCREEN CARD AND ROUTE
-                        // Card(
-                        //   color: Colors.orange,
-                        //   child: Column(
-                        //     children: [
-                        //       Container(
-                        //         height: 200,
-                        //         width: 200,
-                        //         child: InkWell(
-                        //           onTap: () {
-                        //             Navigator.pushNamed(context, '/third');
-                        //           },
-                        //           child: Material(
-                        //             color: _color5,
-                        //             elevation: 10.0,
-                        //             borderRadius: BorderRadius.circular(35.0),
-                        //             child: Container(
-                        //               child: Column(
-                        //                 children: <Widget>[
-                        //                   Padding(
-                        //                       padding: EdgeInsets.all(8),
-                        //                       child: Material(
-                        //                         elevation: 5.0,
-                        //                         borderRadius:
-                        //                             BorderRadius.circular(100.0),
-                        //                         child: Container(
-                        //                           height: 150.0,
-                        //                           width: 150.0,
-                        //                           child: ClipOval(
-                        //                               child: Image.asset(
-                        //                                   'images/about.png')),
-                        //                         ),
-                        //                       )),
-                        //                   Center(
-                        //                     child: Text(
-                        //                       " Developer",
-                        //                       style: TextStyle(
-                        //                         fontSize: 20.0,
-                        //                         color: Colors.black,
-                        //                         fontFamily: 'LexendDeca-Regular',
-                        //                         fontWeight: FontWeight.w700,
-                        //                       ),
-                        //                     ),
-                        //                   ),
-                        //                 ],
-                        //               ),
-                        //             ),
-                        //           ),
-                        //         ),
-                        //       ),
-                        //     ],
-                        //   ),
-                        // ),
                       ]),
                 )),
           ],
